3e415b4667618d3a8e2f94472d88cf981dd71713
Andrew Sage <andrew@asae.co.uk>
2015-01-13 12:45:24 +0000

    Reconfigured for Brechin

--- config/initializers/app_settings.rb
+++ config/initializers/app_settings.rb
@@ -1,4 +1,4 @@
-APP_NAME      = 'Edinburgh Collected'
-CONTACT_EMAIL = 'informationdigital@edinburgh.gov.uk'
-CITY          = 'Edinburgh'
+APP_NAME      = 'Brechin Collected'
+CONTACT_EMAIL = 'brechin@example.com'
+CITY          = 'Brechin'
 COUNTRY       = 'United Kingdom'
